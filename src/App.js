43f8a31a1134a6d01a73dad591133b9f9e8bdbe6
kevinfun <kevinfang.kf@gmail.com>
2023-02-25 11:31:55 +0800

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [fetchData, setFetch] = useState(false);
+
+  useEffect(() => {
+    if(fetchData) {
+      makeAPICall();
+    }
+  }, [fetchData]);
+
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/http://120.79.165.40/fetchWechatTicket', {
+        method: "GET",
+        headers : {
+          "Content-Type" : "application/json",
+        }
+      });
+      const jsonResp = await response.json();
+      setData(jsonResp)
+    } catch (e) {
+        console.log(e);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setFetch(true)}>Fetch Data</button>
+        <p>{JSON.stringify(data)}</p>
       </header>
     </div>
   );
